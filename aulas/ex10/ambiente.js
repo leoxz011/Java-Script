a8641350dd0121a45ef3a3c907c5ee5d619b200c
LeonardoCardoso <lc938887@gmail.com>
2024-02-20 14:29:06 -0300

    exercicio de javascript

--- aulas/ex10/ambiente.js
+++ aulas/ex10/ambiente.js
@@ -4,9 +4,9 @@ num[6] = 0
 console.log(`O vetor tem ${num.length} valores.`)
 console.log(`Os valores são ${num.sort()}`)
 console.log(`O primeiro valor é ${num[0]} e o ultimo é ${num[6]}`)
-let pos = num.indexOf(9)
+let pos = num.indexOf(5)
 if(pos == 0){
     console.log('Valor não encontrado.')
 } else {
-    console.log(`a posição do valor é ${pos}`)
+    console.log(`a posição do valor é ${pos}°`)
 }
---
+++ aulas/ex12/sa.js
@@ -0,0 +1,8 @@
+let num = document.getElementById('numero')
+let lista = document.getElementById('select')
+let res = document.getElementById('resposta')
+let valores = []
+
+function adicionar(){
+    if(isnumero(num.value) && !inlista(num.value))
+}
