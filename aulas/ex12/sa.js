22d591ef979eb0fcce2186e6cb06bd4b6a8687e7
LeonardoCardoso <lc938887@gmail.com>
2024-02-22 10:12:07 -0300

    .

--- aulas/ex12/sa.js
+++ aulas/ex12/sa.js
@@ -1,46 +1,39 @@
-let num = document.getElementById('numero')
-let lista = document.getElementById('select')
-let res = document.getElementById('resposta')
-let valores = []
+const num = document.getElementById('numero');
+const lista = document.getElementById('select');
+const res = document.getElementById('resposta');
+const valores = [];
 
-
-function isnumero(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
+function isNumero(n) {
+    return Number(n) >= 1 && Number(n) <= 100;
 }
 
-function inlista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
+function inLista(n, l) {
+    return l.includes(Number(n));
 }
 
 function adicionar() {
-    if (isnumero(num.value) && !inlista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `O valor ${num.value} foi adicionado.`
-        lista.appendChild(item)
-        res.innerHTML = ''
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value));
+        const item = document.createElement('option');
+        item.text = `O valor ${num.value} foi adicionado.`;
+        lista.appendChild(item);
     } else {
-        alert('Número invalido ou já adicionado!')
+        alert('Número inválido ou já adicionado!');
     }
-    num.value = ''
-    num.focus()
+    num.value = '';
+    num.focus();
 }
 
-function finalizar(){
-    if(valores.length == 0){
-    alert('Adicionar valores antes de finalizar')
- } else {
-    let tot = valores.length
+function finalizar() {
+    if (valores.length == 0) {
+alert('adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
 
-    res.innerHTML = ''
-    res.innerHTML += `<p>Ao todo, temos ${tot} valores cadastrados.</p>`
- }
+
+
+        res.innerHTML =''
+        res.innerHTML += `Ao todo, temos ${tot} valores cadastrados.`
+    }
+
 }
